2374c59796f879464de5d46aafc6b6b811fce504
pYassine <humans@MacBook-Pro-de-Humans.local>
2016-03-25 14:18:47 +0100

    Vérification du mail existant

--- actions/users/create.js
+++ actions/users/create.js
@@ -1,16 +1,36 @@
 var sha1 = require('sha1');
 
 module.exports = function(server){
-  return function(req, res, next){
-    req.body.password = sha1(req.body.password);
+	return function(req, res, next){
+		req.body.password = sha1(req.body.password);
 
-    var User = server.models.User;
-    console.log(req.body);
-    var user = new User(req.body);
-    user.save(function(err, data){
-        if (err)
-          return res.status(500).send(err);
-        res.send(data);
-      });
-  };
+		var User = server.models.User;
+		console.log(req.body);
+
+		User.findOne({
+			email: req.body.email
+		}, function (err, data) {
+			if (err) {
+				return res.status(500).send(err);
+			}
+
+			if (!data) {
+				var user = new User(req.body);
+				user.save(function(err, data){
+					if (err)
+						return res.status(500).send(err);
+					res.send(data);
+				});
+			}
+			else {
+
+				var err = "Email déjà existant";
+				console.log(err);
+				return res.status(401).send(err);
+
+			}
+
+		});
+
+	};
 }
